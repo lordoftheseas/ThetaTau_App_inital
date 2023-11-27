6f0ba0a4f44dce48f5009904e90080d062dd5687
William Chen <wwilliamchenn@gmail.com>
2023-11-26 21:06:05 -0500

    Finished Brothers Profile Pics Co-authored-by: Suh716 <Suh716@users.noreply.github.com>

--- components/BrothersScreen.js
+++ components/BrothersScreen.js
@@ -15,6 +15,38 @@ const getImageSource = (name) => {
   switch(name) {
     case 'Aryaman Ghosh': return require('../assets/Brothers/aryaman_ghosh.png');
     case 'Gabriel Thornton': return require('../assets/Brothers/gabriel.png');
+    case 'Marissa Gabbamonte': return require('../assets/Brothers/Marissa_Gabbamonte.jpeg');
+    case 'Yuanjie Xu': return require('../assets/Brothers/yuanjie.png');
+    case 'Karen Cheng': return require('../assets/Brothers/3Karen_Cheng.png');
+    case 'Daniel Klein': return require('../assets/Brothers/daniel_klein.png');
+    case 'Lior Adjudanpor': return require('../assets/Brothers/3Lior_Adjudanpor.png');
+    case 'Matt Amato': return require('../assets/Brothers/matthew_amato.png');
+    case 'William Bizier': return require('../assets/Brothers/3William_Bizier.png');
+    case 'Lydia Cenac': return require('../assets/Brothers/lydia.png');
+    case 'Elise Dougherty': return require('../assets/Brothers/eliseD.png');
+    case 'Daniel Galdamez': return require('../assets/Brothers/daniel_galdamez.png');
+    case 'Abel George': return require('../assets/Brothers/3Abel_George.png');
+    case 'Max Gustin': return require('../assets/Brothers/max_gustin.png');
+    case 'James Henry': return require('../assets/Brothers/jph.png');
+    case 'Bushra Khan': return require('../assets/Brothers/3Bushra_Khan.png');
+    case 'Ryan Khan': return require('../assets/Brothers/3Ryan_Khan.png');
+    case 'Anna Kane': return require('../assets/Brothers/anna_kane.png');
+    case 'Lana Kim': return require('../assets/Brothers/3Lana_Kim.jpg');
+    case 'Jeffrey Kou': return require('../assets/Brothers/jeffrey_kou.png');
+    case 'Edward Masalimov': return require('../assets/Brothers/edward.png');
+    case 'Muhtasim Mushfiq': return require('../assets/Brothers/3Muhtasim_Mushfiq.png');
+    case 'Isabella Nguyen': return require('../assets/Brothers/bella.png');
+    case 'Carlos Pedraza': return require('../assets/Brothers/3Carlos_Pedraza.png');
+    case 'Quentin Rotoli': return require('../assets/Brothers/3Quentin_Rotoli.png');
+    case 'Adrian Sin': return require('../assets/Brothers/adrian_sin.png');
+    case 'Rrucha Singh': return require('../assets/Brothers/rrucha_singh.png');
+    case 'Justin Siwa': return require('../assets/Brothers/justin_siwa.png');
+    case 'Chloe Tullius': return require('../assets/Brothers/chloe_tullius.png');
+    case 'Michael VanAuken': return require('../assets/Brothers/3Michael_VanAuken.png');
+    case 'Mathew Yeung': return require('../assets/Brothers/3Mathew_Yeung.png');
+    case 'Jenny Zhu': return require('../assets/Brothers/jenny.png');
+    case 'Hao Zhu': return require('../assets/Brothers/hao_zhu.png');
+    
     // Add cases for each brother's name
     default: return require('../assets/Brothers/gabriel.png'); // A default image if no match is found
   }
